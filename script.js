a3a4254fa53783875cd98582f48dcd0da7b8ffd0
Rudy Brandies <adanb82@gmail.com>
2023-10-06 14:09:07 -0500

    added a loader between quote fetches

--- script.js
+++ script.js
@@ -3,12 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 let apiQuotes = [];
 
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 function newQuote() {
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    loading();
+    setTimeout(() => {
+        const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
     if(quote.author === null) {
         authorText.textContent = 'Unknown'
@@ -25,10 +38,15 @@ function newQuote() {
     }
     
     quoteText.textContent = quote.text;
+    complete();
+    }, 1200);
+    
     
 }
 
 async function getQuotes() {
+    loading();
+    
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
